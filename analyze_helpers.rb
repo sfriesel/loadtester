a5deb3baeea7e487007e4c02c133723ec6f9f9c5
Ivan Kusalic <ikusalic@gmail.com>
2013-03-26 14:53:46 +0100

    Refactor analyze scripts

--- analyze_helpers.rb
+++ analyze_helpers.rb
@@ -1,12 +1,15 @@
-def show_stats(d, prefix="")
+def show_stats(d, options={})
   n = d.size
+  prefix = options[:prefix] || ""
+  unit = options[:unit] || ""
+
   puts "#{ prefix }number of entries: #{ n } #"
 
   avg = d.reduce(:+) / n
-  puts "#{ prefix }average: #{ avg } s"
+  puts "#{ prefix }average: #{ avg } #{ unit }"
 
   med = d[n / 2]
-  puts "#{ prefix }median: #{ med } s"
+  puts "#{ prefix }median: #{ med } #{ unit }"
 
   std_dev = (d.reduce(0) { |acc, e| acc + (e - avg) ** 2 } / n) ** 0.5
   puts "#{ prefix }standard deviation: #{ std_dev }"
