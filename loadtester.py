c200b66bc415962dded05b7f41da8f16ddbcb0de
Stefan Friesel <sf@cloudcontrol.de>
2013-04-09 21:12:59 +0200

    debug

--- loadtester.py
+++ loadtester.py
@@ -63,7 +63,11 @@ class Browser(gevent.Greenlet):
                     status=response.status))
 
         if response.status == 200 and response.getheader('content-type').startswith('text/'):
-            subresults = gevent.pool.Group().imap(self.make_request, iter(iter(response.data.split('\n')).next, ''))
+            try:
+                subresults = gevent.pool.Group().imap(self.make_request, iter(iter(response.data.split('\n')).next, ''))
+            except Exception:
+                sys.stderr.write('>>>' + response.data + '<<<')
+                raise
         else:
             subresults = []
 
